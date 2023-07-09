737b28c5132ae4c32a8ce80916745df3a321fa18
valilinn <valentinalinchuk@gmail.com>
2023-07-09 19:43:07 +0200

    Cells created

---
+++ Race/Race/Cells/WinnersStatsTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  WinnersStatsTableViewCell.swift
+//  Race
+//
+//  Created by Валентина Лінчук on 09/07/2023.
+//
+
+import UIKit
+
+class WinnersStatsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
