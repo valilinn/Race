6d569bb6def9f4f3bcd0a9c461ac44ac00189bbe
valilinn <valentinalinchuk@gmail.com>
2023-07-16 18:22:02 +0200

    General image changed

--- Race/Race/AppDelegate.swift
+++ Race/Race/AppDelegate.swift
@@ -13,7 +13,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        // Color of tint
+        UINavigationBar.appearance().tintColor = UIColor.gray
+        UINavigationBar.appearance().titleTextAttributes = [NSAttributedString.Key.foregroundColor : UIColor.gray]
         return true
     }
 
