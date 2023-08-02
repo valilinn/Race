170eff8fff844f51f25fca080738e4b875443001
valilinn <valentinalinchuk@gmail.com>
2023-08-02 14:26:06 +0200

    Car animation added

--- Race/Race/Controllers/RaceViewController.swift
+++ Race/Race/Controllers/RaceViewController.swift
@@ -9,15 +9,9 @@ import UIKit
 
 class RaceViewController: UIViewController {
     
-    //MARK: - Additional Types
-    enum ElementPosition {
-        case left, center, right
-    }
     
     //MARK: - UI Elements
     
-    
-//    var carImage = UIImageView(image: UIImage(named: "carTop"))
     var carImage = UIImageView(image: UIImage(named: "mainCar"))
     var treeImageGreen = UIImageView(image: UIImage(named: "tree"))
     var treeImageBlack = UIImageView(image: UIImage(named: "treeBlack"))
@@ -37,7 +31,6 @@ class RaceViewController: UIViewController {
     var centerOriginCoordinate: CGFloat = 0
     var rightOriginCoordinate: CGFloat = 0
     
-    //car size
     var elementSize: CGFloat = 0
     var defaultPadding: CGFloat = 10
 
@@ -65,36 +58,31 @@ class RaceViewController: UIViewController {
        
     }
     
+    
     @objc
     func carGestureAction(sender: UISwipeGestureRecognizer) {
-        if carImage.frame.origin.x == centerOriginCoordinate {
+        var destinationCoordinate = carImage.frame.origin.x
+        if destinationCoordinate == centerOriginCoordinate {
             switch sender.direction {
             case .left:
-                carImage.frame.origin.x = leftOriginCoordinate
-//                moveRockTo(.left)
-//                moveGreenTreeTo(.left)
-//                moveRocksTo(.left)
-//                moveBlackTreeTo(.left)
-//                moveTreeBushTo(.left)
+                destinationCoordinate = leftOriginCoordinate
             case .right:
-                carImage.frame.origin.x = rightOriginCoordinate
-//                moveRockTo(.center)
-//                moveGreenTreeTo(.center)
-//                moveRocksTo(.center)
-//                moveBlackTreeTo(.center)
-//                moveTreeBushTo(.center)
-                
+                destinationCoordinate = rightOriginCoordinate
             default:
                 break
             }
         } else  {
-            carImage.frame.origin.x = centerOriginCoordinate
-//            moveRockTo(.right)
-//            moveGreenTreeTo(.right)
-//            moveRocksTo(.right)
-//            moveBlackTreeTo(.right)
-//            moveTreeBushTo(.right)
+            destinationCoordinate = centerOriginCoordinate
+
         }
+        
+        UIView.animate(
+            withDuration: 0.3,
+            delay: 0,
+            options: [.curveEaseIn],
+            animations: { [weak self] in
+                self?.carImage.frame.origin.x = destinationCoordinate
+            })
     }
     
     
@@ -106,7 +94,6 @@ class RaceViewController: UIViewController {
         topSafeAreaPadding = UIApplication.shared.windows.first?.safeAreaInsets.top ?? 0
        navigationBarHeight = navigationController?.navigationBar.frame.size.height ?? 0
 
-        
         elementSize = screenWidth / 4
         leftOriginCoordinate = elementSize / 4
         centerOriginCoordinate = elementSize + 2 * leftOriginCoordinate
@@ -114,7 +101,6 @@ class RaceViewController: UIViewController {
     }
     
     func setupFrames() {
-        
         setupCar()
         setupTreeGreen()
         setupTreeBlack()
@@ -194,72 +180,5 @@ class RaceViewController: UIViewController {
         view.addSubview(rocksImage)
     }
     
-//MARK: - Moves
-    
-    func moveCarTo(_ position: ElementPosition) {
-        switch position {
-        case .left:
-            carImage.frame.origin.x = leftOriginCoordinate
-        case .center:
-            carImage.frame.origin.x = centerOriginCoordinate
-        case .right:
-            carImage.frame.origin.x = rightOriginCoordinate
-        }
-    }
-    
-    func moveRockTo(_ position: ElementPosition) {
-        switch position {
-        case .left:
-            rockImage.frame.origin.x = centerOriginCoordinate
-        case .center:
-            rockImage.frame.origin.x = rightOriginCoordinate
-        case .right:
-            rockImage.frame.origin.x = leftOriginCoordinate
-        }
-    }
-    
-    func moveGreenTreeTo(_ position: ElementPosition) {
-        switch position {
-        case .left:
-            treeImageGreen.frame.origin.x = rightOriginCoordinate
-        case .center:
-            treeImageGreen.frame.origin.x = centerOriginCoordinate
-        case .right:
-            treeImageGreen.frame.origin.x = leftOriginCoordinate
-        }
-    }
-    
-    func moveRocksTo(_ position: ElementPosition) {
-        switch position {
-        case .left:
-            rocksImage.frame.origin.x = centerOriginCoordinate
-        case .center:
-            rocksImage.frame.origin.x = rightOriginCoordinate
-        case .right:
-            rocksImage.frame.origin.x = centerOriginCoordinate
-        }
-    }
-    
-    func moveBlackTreeTo(_ position: ElementPosition) {
-        switch position {
-        case .left:
-            treeImageBlack.frame.origin.x = leftOriginCoordinate
-        case .center:
-            treeImageBlack.frame.origin.x = leftOriginCoordinate
-        case .right:
-            treeImageBlack.frame.origin.x = centerOriginCoordinate
-        }
-    }
-    
-    func moveTreeBushTo(_ position: ElementPosition) {
-        switch position {
-        case .left:
-            treeImageBush.frame.origin.x = rightOriginCoordinate
-        case .center:
-            treeImageBush.frame.origin.x = leftOriginCoordinate
-        case .right:
-            treeImageBush.frame.origin.x = leftOriginCoordinate
-        }
-    }
     
 }
